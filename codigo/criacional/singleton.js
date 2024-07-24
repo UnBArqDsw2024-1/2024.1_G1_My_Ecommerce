a1f3c5c0d3ec06cc89e72f53c88433c6fe9eea8e
Marcus Martins <vini.mark2002@gmail.com>
2024-07-24 01:23:02 -0300

    Aprimorando encapsulamento e visualização dos dados
    Co-authored-by: Pedro Barbosa <pedrobarbosaocb@gmail.com>
    
    - Foram usados gets e sets na visualização e definição dos atributos, melhorando o encapsulamento
    
    - Foi adicionada uma estrutura try-catch para facilitar debug
    
    - Foram adicionadas saídas de visualização mais significativa para  melhor comparação
    
    - Foram adicionados protótipos comentados das funções presentes nos diagramas

--- codigo/criacional/singleton.js
+++ codigo/criacional/singleton.js
@@ -10,13 +10,29 @@ class Catalogo {
         return Catalogo._instance;
     }
 
-    addJogo(jogo){
+    addJogoAoCarrinho(jogo) {
         this._jogos.push(jogo);
+    }
+
+    /*
+    buscarJogo(nome) {
+        return this._jogosPossuidos.find(jogo => jogo._nome === nome);
+        }
+        
+    filtrarJogos(criterios) {
+        return this._jogosPossuidos.filter(jogo => {
+            return Object.keys(criterios).every(key => jogo[key] === criterios[key]);
+            });
+    }
+    */
+
+    get jogos() {
+        return this._jogos;
     }
 }
 
 class Jogo {
-    constructor(nome, desenvolvedor, editora, preco){
+    constructor(nome, desenvolvedor, editora, preco) {
         this._nome = nome;
         this._desenvolvedor = desenvolvedor;
         this._editora = editora;
@@ -24,20 +40,24 @@ class Jogo {
     }
 }
 
-// Criando tres Catalogos
-const catalogo1 = Catalogo.getInstance();
-const catalogo2 = Catalogo.getInstance();
-const catalogo3 = new Catalogo();
+try {
+    // Criando tres Catalogos
+    const catalogo1 = Catalogo.getInstance();
+    const catalogo2 = Catalogo.getInstance();
+    const catalogo3 = new Catalogo(); // Este caso deve falhar
 
-// Checando os catalogos antes de modificar
-console.log('Catalogo 1 antes:', JSON.stringify(catalogo1));
-console.log('Catalogo 2 antes:', JSON.stringify(catalogo2));
-console.log('Catalogo 3 antes:', JSON.stringify(catalogo3));
+    // Checando os catalogos antes de modificar
+    console.log('Catalogo 1 antes:', catalogo1.jogos);
+    console.log('Catalogo 2 antes:', catalogo2.jogos);
+    console.log('Catalogo 3 antes:', catalogo3.jogos);
 
-// Modificando um catalogo
-catalogo1.addJogo(new Jogo('Valorant', 'Riot Games', 'Riot Games', 'R$20,00'));
+    // Modificando um catalogo
+    catalogo1.addJogoAoCarrinho(new Jogo('Valorant', 'Riot Games', 'Riot Games', 'R$20,00'));
 
-// Checando os catalogos depois de modificar
-console.log('Catalogo 1 depois:', JSON.stringify(catalogo1));
-console.log('Catalogo 2 depois:', JSON.stringify(catalogo2));
-console.log('Catalogo 3 depois:', JSON.stringify(catalogo3));
+    // Checando os catalogos depois de modificar
+    console.log('Catalogo 1 depois:', catalogo1.jogos);
+    console.log('Catalogo 2 depois:', catalogo2.jogos);
+    console.log('Catalogo 3 depois:', catalogo3.jogos);
+} catch (error) {
+    console.error(error.message);
+}
