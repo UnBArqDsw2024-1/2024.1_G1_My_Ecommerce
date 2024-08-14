ed8c9642ef0b81f41fe974aa369f966c279f4949
raqueleucaria <raqueleucaria@gmail.com>
2024-08-14 18:05:00 -0300

    add:  funcao para cliente logar

--- app/backend/src/main.ts
+++ app/backend/src/main.ts
@@ -1,13 +1,18 @@
 import { ApiExpress } from "./api/express/api.express";
+import { ClienteControladora } from "./api/express/controladora/cliente.controladora";
 import { JogoControladora } from "./api/express/controladora/jogo.controladora";
 
 function main() {
     const api = ApiExpress.build();
 
-    const controller = JogoControladora.build();
+    const controllerJogo = JogoControladora.build();
+    const controllerCliente = ClienteControladora.build();
 
-    api.addGetRoute("/jogos", controller.lista);
-    api.addPostRoute("/jogos/pesquisa", controller.pesquisarPorNome);
+    api.addGetRoute("/jogos", controllerJogo.lista);
+    api.addPostRoute("/jogos/pesquisa", controllerJogo.pesquisarPorNome);
+    api.addGetRoute("/clientes", controllerCliente.listar);
+    // api.addPostRoute("/clientes/cadastrar", controllerCliente.cadastrar);
+    api.addPostRoute("/clientes/logar", controllerCliente.logar);
 
     api.start(8000);
 }
